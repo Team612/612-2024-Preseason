5d67cd3ad4d297f96187eecdcfdc32d68bb14c09
AchyutD <achyutdipu@gmail.com>
2023-11-13 20:41:55 -0500

    updated intake.java

--- robot-code/src/main/java/frc/robot/subsystems/Intake.java
+++ robot-code/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,24 +1,34 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-// Unfinished
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import edu.wpi.first.wpilibj.Compressor;
-
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
+  DoubleSolenoid exampleDoublePCM;
+  DoubleSolenoid exampleDoublePH;
   public Intake() {
-    Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
-    Compressor phCompressor = new Compressor(1, PneumaticsModuleType.REVPH);
-    pcmCompressor.enableDigital();
-    phCompressor.disable();
-    boolean enabled = pcmCompressor.enabled();
-    boolean pressureSwitch = pcmCompressor.getPressureSwitchValue();
+    exampleDoublePCM = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+    exampleDoublePH = new DoubleSolenoid(9, PneumaticsModuleType.REVPH, 4, 5);
+  }
+  public void setSolenoidStatus(int solenoidStatus) {
+    if (solenoidStatus == 0) {
+      exampleDoublePCM.set(kOff);
+    } else if (solenoidStatus == 1) {
+      exampleDoublePCM.set(kForward);
+    } else if (solenoidStatus == 2) {
+      exampleDoublePCM.set(kReverse);
+    } else {
+      for (var i = 3; i <= solenoidStatus; i++) {
+        exampleDoublePCM.toggle();
+      }
+    }
   }
 
   @Override
