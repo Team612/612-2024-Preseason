793a0ab5b3e18e68f79d0a440335e69fe937efd2
612-programming <ttylerpro@gmail.com>
2023-11-27 19:28:17 -0500

    pp

--- robot-code/src/main/java/frc/robot/commands/Autonomous/TurnTowardsObject.java
+++ robot-code/src/main/java/frc/robot/commands/Autonomous/TurnTowardsObject.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Limelight;
-import frc.robot.subsystems.Vision;
 
 public class TurnTowardsObject extends CommandBase {
   private final ProfiledPIDController turnController = Constants.VisionConstants.rotationController;
